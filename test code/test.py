c18e7a7ce2d772e3afb5d0c4b47de767fd0c9d29
lkjhui1151 <lkjhui1151@gmail.com>
2022-02-22 14:20:41 +0700

    update

--- test code/test.py
+++ test code/test.py
@@ -9,11 +9,25 @@ doc = DocxTemplate("D:/github/WebReport/test.docx")
 Content = {
     'name': [
         {
-            'name2': "ssss"
+            'address': "192.168.1.1"
+        },
+        {
+            'address': "192.168.1.3"
+        },
+        {
+            'address': "192.168.1.2"
         }
     ]
 }
 
-doc.render(Content)
-doc.save("generated_doc.docx")
-os.system("generated_doc.docx")
+# print(Content['name'][0]['address'])
+
+# number = []
+# for i in range(len(Content['name'])):
+items = sorted(Content['name'], key=lambda d: tuple(map(int, d['address'].split('.'))))
+
+print(items)
+
+# doc.render(Content)
+# doc.save("generated_doc.docx")
+# os.system("generated_doc.docx")
