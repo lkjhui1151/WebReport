548d73719a06cb7c6d143c7d72f2163c02a40d9c
lkjhui1151 <lkjhui1151@gmail.com>
2022-03-18 16:39:11 +0700

    test hash

--- testCode/Nodejs/index.js
+++ testCode/Nodejs/index.js
@@ -1,17 +1,12 @@
-let example1 = [5, 7, 6];
+const crypto = require("crypto");
 
-example1.push(8, 9, 10);
-//The more number u add to the push code, more number will add on the sequence
-example1.pop();
-//Will remove the last number in the sequence
+var hash = crypto.createHash('sha512');
 
 
-example1[0] = 1;
-//Overwrite values Target values and reset the value on that index 0
+function func(string) {
+    data = hash.update(string).digest('hex');
+    // gen_hash = data;
+    console.log(data);
+}
 
-example1.forEach((element) => {
-    console.log(element)
-});
-
-//The entire array use a forEach statement
-console.log(example1)
+func("ABCD")
--- testCode/testCode.py
+++ testCode/testCode.py
@@ -1,68 +1,10 @@
-import csv
-from importlib.resources import contents
-import json
-from docxtpl import *
-import pandas
+# Hash a single string with hashlib.sha256
+import hashlib
 
-path = "D:/github/WebReport/testCode/"
-inputCSV = "nmap.csv"
-doc = DocxTemplate(path+"nmap.docx")
+with open("D:/github/WebReport/testCode/hash.txt", 'rb') as f:
+    for line in f:
+        hashed_line = hashlib.sha512(line).hexdigest()
+        print(hashed_line)
 
-
-def makeJson(csvFilePath, jsonFilePath):
-    data = {}
-    with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
-        key_id = 0
-        for rows in csvReader:
-            key = key_id
-            data[key] = rows
-            key_id += 1
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-        jsonf.write(json.dumps(data, indent=4))
-
-csvFilePath = path+inputCSV
-jsonFilePath = path+'nmap.json'
-makeJson(csvFilePath, jsonFilePath)
-
-DataJSON = pandas.read_json(jsonFilePath)
-dict_IP_port = {}
-temp_IP = "a"
-list_port = []
-list_port_prot_serv = []
-dict_port_prot_serv = {}
-
-for i in DataJSON:
-    if DataJSON[i]['host__address__addr'] != "":
-        dict_IP_port[temp_IP] = list_port
-        temp_IP = DataJSON[i]['host__address__addr']
-        list_port = []
-    list_port.append(DataJSON[i]['host__ports__port__portid'])
-# ======================================================================================================
-    if DataJSON[i]['host__ports__port__state__state'] == "open":
-        dict_port_prot_serv["port"] = DataJSON[i]['host__ports__port__portid']
-        dict_port_prot_serv["protocol"] = DataJSON[i]['host__ports__port__protocol']
-        dict_port_prot_serv["service"] = DataJSON[i]['host__ports__port__service__name']
-        list_port_prot_serv.append(dict_port_prot_serv)
-        dict_port_prot_serv = {}
-
-dict_IP_port[temp_IP] = list_port
-del dict_IP_port['a']
-
-seen = set()
-new_l = []
-for d in list_port_prot_serv:
-    t = tuple(d.items())
-    if t not in seen:
-        seen.add(t)
-        new_l.append(d)
-
-list_port_prot_serv = (sorted(new_l, key=lambda x: int(x['port'])))
-# =======================================================================================================
-
-
-contents = {}
-contents['nmap_port'] = list_port_prot_serv
-
-doc.render(contents)
-doc.save(path+"generated_nmap.docx")
+"49ec55bd83fcd67838e3d385ce831669e3f815a7f44b7aa5f8d52b5d42354c46d89c8b9d06e47a797ae4fbd22291be15bcc35b07735c4a6f92357f93d5a33d9b"
+"49ec55bd83fcd67838e3d385ce831669e3f815a7f44b7aa5f8d52b5d42354c46d89c8b9d06e47a797ae4fbd22291be15bcc35b07735c4a6f92357f93d5a33d9b"
