278993d01b078a22e184ccbe85b0f271b346398a
KriT UPanun <35343051+kiujkio@users.noreply.github.com>
2022-04-04 10:27:48 +0700

    report SOC but no inc

--- code_krit/report_SOC/test.py
+++ code_krit/report_SOC/test.py
@@ -19,6 +19,7 @@ mydb = mysql.connector.connect(
     database="inetms_autoreport"
 )
 
+
 def dictfetchall(cursor):
     # Return all rows from a cursor as a dict
     columns = [col[0] for col in cursor.description]
@@ -26,44 +27,51 @@ def dictfetchall(cursor):
         dict(zip(columns, row))
         for row in cursor.fetchall()
     ]
-def create_index(list,DB):
-    temp_index = 1 
+
+
+def create_index(list, DB):
+    temp_index = 1
     for i in DB:
         i['no'] = temp_index
         list.append(i)
-        temp_index+=1
+        temp_index += 1
     return(list)
 
-path = 'D:/INET-MS/Auto report/GitHub/WebReport/code_krit/report_SOC/'
-start_date ='2022-03-01' 
-end_date = '2022-04-01'
 
+path = 'D:/INET-MS/Auto report/GitHub/WebReport/code_krit/report_SOC/'
+start_date = '2022-03-01'
+end_date = '2022-04-01'
+month_report = "มีนาคม 2565"
+cus_name = "xspringam"
 mycursor = mydb.cursor()
-cus_name= "xspringam"
 
 sql = "select category,name_en,detail_th,priority from vulnerability"
 mycursor.execute(sql)
 vulnerability_DB = dictfetchall(mycursor)
 
-sql = "select name,address,remark,type,name_th from device join company_list ON device.company_id = company_list.id join device_type ON device_type.id = device.device_id where company_list.initials = \""+cus_name+"\""
+sql = "select name,address,remark,type,name_th,service_provider from device join company_list ON device.company_id = company_list.id join device_type ON device_type.id = device.device_id where company_list.initials = \""+cus_name+"\""
 mycursor.execute(sql)
 device_DB = dictfetchall(mycursor)
 
-sql = "select AVG(value) from capa_daily_log join company_list ON company_list.id = capa_daily_log.company_id where company_list.initials = \""+cus_name+"\" and capa_daily_log.date between \""+start_date+"\" and \""+end_date+"\""
+sql = "select AVG(value) from capa_daily_log join company_list ON company_list.id = capa_daily_log.company_id where company_list.initials = \"" + \
+    cus_name+"\" and capa_daily_log.date between \"" + \
+    start_date+"\" and \""+end_date+"\""
 mycursor.execute(sql)
 capa_DB = dictfetchall(mycursor)
 
-device  =   []
+device = []
 vulnerability = []
-create_index(device,device_DB)
-create_index(vulnerability,vulnerability_DB)
+create_index(device, device_DB)
+create_index(vulnerability, vulnerability_DB)
 
-contents={}
-contents['name_capany'] =   device_DB[0]['name_th']
-contents['inet_create'] =   'บริษัท อินเทอร์เน็ตประเทศไทย จำกัด (มหาชน)'
-contents['capa']    =   '{:.4f}'.format(float(capa_DB[0]['AVG(value)'])) 
-contents['vulnerability']   =   vulnerability
-contents['device']  =     device
+contents = {}
+contents['month_report'] = month_report
+contents['Service_provider'] = "บริษัท อินเทอร์เน็ตประเทศไทย จำกัด (มหาชน)" if device_DB[0]['service_provider'] == "INET" else "บริษัท ไอเน็ต แมเนจด์ เซอร์วิสเซส จำกัด"
+contents['name_capany'] = device_DB[0]['name_th']
+contents['inet_create'] = 'บริษัท อินเทอร์เน็ตประเทศไทย จำกัด (มหาชน)'
+contents['capa'] = '{:.4f}'.format(float(capa_DB[0]['AVG(value)']))
+contents['vulnerability'] = vulnerability
+contents['device'] = device
 
 doc = DocxTemplate(path+"templateSOC.docx")
 
