b41805e28f8023f284fed8fcb3667402e7777b1f
KriT UPanun <35343051+kiujkio@users.noreply.github.com>
2022-04-28 09:53:59 +0700

    update report SOC
    code_krit  > report_SOC > report_SOC

--- code_krit/report_SOC/report_SOC.py
+++ code_krit/report_SOC/report_SOC.py
@@ -1,10 +1,20 @@
 from ast import Global
+from calendar import month
 from logging import critical
+from pickle import TRUE
 from statistics import median
 from turtle import circle
 from executing import Source
 import mysql.connector
 from docxtpl import *
+import calendar
+from numpy import imag
+from openpyxl import Workbook
+from openpyxl.chart import BarChart, Reference
+from openpyxl.chart.label import DataLabelList
+from openpyxl.chart.shapes import GraphicalProperties
+from win32com.client import Dispatch
+
 
 mydb = mysql.connector.connect(
     host="10.11.101.32",
@@ -42,14 +52,16 @@ def create_index(list, DB):
 
 path = 'D:/INET-MS/Auto report/GitHub/WebReport/code_krit/report_SOC/'
 doc = DocxTemplate(path+"templates/templateSOC.docx")
-
-
+# =========================================================INPUT===========================================================
 start_date = '2022-03-01'
 end_date = '2022-04-01'
-month_report = "มีนาคม 2565"
+month = "มีนาคม"
+year = "2565"
 cus_name = "xspringam"
 
+# ===========================================================Query=============================================================
 mycursor = mydb.cursor()
+
 
 sql = "select category,name_en,detail_th,priority from vulnerability"
 mycursor.execute(sql)
@@ -70,18 +82,33 @@ sql = "select * from trend join vulnerability on vulnerability.name_en = trend.t
 mycursor.execute(sql)
 trend_DB = dictfetchall(mycursor)
 
+temp = str(int(year)-543)
+sql = "select type,priority,last_datetime from trend join vulnerability on vulnerability.name_en = trend.type join company_list ON company_list.id =trend.company_id where company_list.initials = \""+cus_name+"\"and trend.last_datetime between \"" + \
+    temp+"-01-01\" and \""+temp+"-12-31\""
+mycursor.execute(sql)
+trend_graph = dictfetchall(mycursor)
 
+# ================================================================Sum event=========================================================
 crit = count_event('Critical (P1)')
 high = count_event('High (P2)')
 medium = count_event('Medium (P3)')
 low = count_event('Low (P4)')
 sumPriority = int(crit)+int(high)+int(medium)+int(low)
-
+# =================================================================Device Table=====================================================
 device = []
 vulnerability = []
 create_index(device, device_DB)
+# =================================================================ภาคผนวก=========================================================
+list_temp = []
+for i in vulnerability_DB:
+    i['sort'] = i['priority'][-2:-1]
+    list_temp.append(i)
+vulnerability_DB = sorted(list_temp, key=lambda d: d['name_en'])
+vulnerability_DB = sorted(vulnerability_DB, key=lambda d: d['category'])
+vulnerability_DB = sorted(vulnerability_DB, key=lambda d: d['sort'])
 create_index(vulnerability, vulnerability_DB)
 
+# ===================================================================detail==========================================================
 head_tbl_vul_map = {'src_address': 'Source IP', 'des_address': 'Destination IP', 'src_geolocation': 'Source Geolocation', 'des_geolocation': 'Destination Geolocation', 'src_user': 'Source User', 'des_user': 'Destination User',
                     'port': 'Port', 'action': 'Event Subtype', 'last_datetime': 'Time', 'protocol': 'Protocol', 'url': 'URL', 'file_name': 'File Name', 'file_path': 'File Path', 'malware_name': 'Malware Name'}
 
@@ -113,6 +140,7 @@ head_tbl_vul = {'Inbound Communication With Blacklist IP Address': ['src_address
                     'src_address', 'des_geolocation', 'port', 'last_datetime'],
                 'Port Scan': ['src_address', 'des_geolocation', 'des_address', 'last_datetime']}
 
+# =======================================================Vul Table==========================================================
 
 dict_group_event = {}
 list_category = []
@@ -151,6 +179,8 @@ count = 0
 for i in list_group_category:
     count += i['count']
 
+# ==========================================================Event Detail==========================================================
+
 all_event_list = []
 head_tbl_vul_new = []
 
@@ -176,36 +206,132 @@ for x in dict_group_event:
     one_event_dict['col_label'] = head_tbl_vul_new
     all_event_list.append(one_event_dict)
 
-contents = {}
+# ==================================================Gen Graph======================================================
+wb = Workbook()
+sheet = wb['Sheet']
+month_graph = []
+data_for_graph = [('Month', 'Type', 'CRITICAL', 'HIGH', 'MEDIUM', 'LOW')]
+dict_graph = {}
+dict_list_graph = {}
+for i in trend_graph:
+    temp = (str(i['last_datetime'])[0:7].split('-'))
+    month_graph = (calendar.month_name[int(temp[1])]) + ' ' + temp[0]
+    if month_graph not in dict_graph:
+        dict_graph[month_graph] = {}
+    if i['type'] not in dict_graph[month_graph]:
+        dict_graph[month_graph][i['type']] = {
+            'CRITICAL': 0, 'HIGH': 0, 'MEDIUM': 0, 'LOW': 0}
 
+    if i['priority'] == 'critical (P1)':
+        dict_graph[month_graph][i['type']]['CRITICAL'] += 1
+    elif i['priority'] == 'High (P2)':
+        dict_graph[month_graph][i['type']]['HIGH'] += 1
+    elif i['priority'] == 'Medium (P3)':
+        dict_graph[month_graph][i['type']]['MEDIUM'] += 1
+    elif i['priority'] == 'Low (P4)':
+        dict_graph[month_graph][i['type']]['LOW'] += 1
+for i, j in dict_graph.items():
+    check = TRUE
+    for x, y in j.items():
+        cri_graph = '' if y['CRITICAL'] == 0 else y['CRITICAL']
+        high_graph = '' if y['HIGH'] == 0 else y['HIGH']
+        med_graph = '' if y['MEDIUM'] == 0 else y['MEDIUM']
+        low_graph = '' if y['LOW'] == 0 else y['LOW']
+
+        if check:
+            temp = (i, x, cri_graph, high_graph, med_graph, low_graph)
+            check = False
+        else:
+            temp = ('', x, cri_graph, high_graph, med_graph, low_graph)
+        data_for_graph.append(temp)
+
+for item in data_for_graph:
+    sheet.append(item)
+
+# declare chart ty Bar
+chart1 = BarChart()
+chart1.type = "col"
+chart1.style = 1
+chart1.title = "Incident Summary "+str(int(year)-543)
+chart1.grouping = "stacked"
+chart1.overlap = 100
+chart1.y_axis.scaling.min = 0
+
+# define chart bar
+data_for_graph = Reference(sheet, min_col=3, min_row=1,
+                           max_row=len(data_for_graph), max_col=6)
+cats = Reference(sheet, min_col=1, min_row=2,
+                 max_row=len(data_for_graph), max_col=2)
+chart1.add_data(data_for_graph, titles_from_data=True)
+chart1.set_categories(cats)
+chart1.shape = 1
+
+# # colors lenend level
+critical_graph = chart1.series[0]
+props = GraphicalProperties(solidFill="7030A0")
+critical_graph.graphicalProperties = props
+
+high_graph = chart1.series[1]
+props = GraphicalProperties(solidFill="FF0000")
+high_graph.graphicalProperties = props
+
+medium_graph = chart1.series[2]
+props = GraphicalProperties(solidFill="FFC000")
+medium_graph.graphicalProperties = props
+
+low_graph = chart1.series[3]
+props = GraphicalProperties(solidFill="70AD47")
+low_graph.graphicalProperties = props
+
+# # set lenend position
+chart1.legend.position = 'b'
+chart1.dataLabels = DataLabelList()
+chart1.dataLabels.showVal = True
+
+# # {'r', 'l', 'b', 'ctr', 'outEnd', 't', 'inBase', 'bestFit', 'inEnd'}
+chart1.dataLabels.position = 'ctr'
+
+# # save chart
+sheet.add_chart(chart1, "M2")
+wb.save('sample.xlsx')
+
+# save image
+app = Dispatch("Excel.Application")
+workbook_file_name = path+'sample.xlsx'
+workbook = app.Workbooks.Open(Filename=workbook_file_name)
+app.DisplayAlerts = False
+
+
+for sheet in workbook.Worksheets:
+    for chartObject in sheet.ChartObjects():
+        chartObject.Chart.Export(path + 'graph.png')
+
+workbook.Close(SaveChanges=False, Filename=workbook_file_name)
+
+# ========================================================Content===============================================
+contents = {}
 img = "inet.png" if device_DB[0]['service_provider'] == "INET" else "inetms.png"
 image = InlineImage(doc, path+img)
 contents['img_provider'] = image
-contents['month_report'] = month_report
+contents['month'] = month
+contents['year'] = year
 contents['Service_provider'] = "บริษัท อินเทอร์เน็ตประเทศไทย จำกัด (มหาชน)" if device_DB[
     0]['service_provider'] == "INET" else "บริษัท ไอเน็ต แมเนจด์ เซอร์วิสเซส จำกัด"
 contents['name_capany'] = device_DB[0]['name_th']
-
 contents['capa'] = '{:.4f}'.format(float(capa_DB[0]['AVG(value)']))
-
 contents['vulnerability'] = vulnerability
-
 contents['device'] = device
-
 contents['crit'] = crit
 contents['medium'] = medium
 contents['low'] = low
 contents['sum'] = sumPriority
-
 contents['high'] = high
-
 contents['all_event'] = all_event_list
-
 contents['category'] = list_group_category
 contents['category_count'] = count
-
-
-image = InlineImage(doc, path+"inet.png")
+graph = InlineImage(doc, path+'graph.png')
+contents['graph'] = graph
 doc.render(contents)
-doc.save(path+"output\SOC.docx")
+doc.save(path+"output\\รายงานสรุปผลการปฏิบัติงานของศูนย์SOC_" +
+         cus_name+'_'+month+'_'+year+".docx")
 # os.system(path+"SOC.docx")
--- code_krit/report_SOC/test.py
+++ code_krit/report_SOC/test.py
@@ -1,44 +1,82 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
-# labels = ['one', 'two', 'tree', 'four', 'five']
-# crit = [0, 0 ,0, 0, 1]
-# high = [0, 0, 0, 0, 0]
-# medium = [1, 1, 4, 2, 5]
-# low = [5, 2, 3, 0,5]
-
-# x = np.arange(len(labels))  # the label locations
-# width = 0.15  # the width of the bars
-
-# fig, ax = plt.subplots()
-# rects1 = ax.bar(x - width*2+width/2, crit, width, label='crit')
-# rects2 = ax.bar(x - width+width/2, high, width, label='high')
-# rects3 = ax.bar(x +width/2, medium, width, label='medium')
-# rects4 = ax.bar(x + width+width/2, low, width, label='low')
-
-# # Add some text for labels, title and custom x-axis tick labels, etc.
-
-# ax.set_title('Incident Summary 2022')
-# ax.set_xticks(x, labels)
-# ax.legend()
-
-# ax.bar_label(rects1, padding=3)
-# ax.bar_label(rects2, padding=3)
-# ax.bar_label(rects3, padding=3)
-# ax.bar_label(rects4, padding=3)
+from openpyxl import Workbook
+from openpyxl.chart import BarChart, Reference
+from openpyxl.chart.label import DataLabelList
+from openpyxl.chart.shapes import GraphicalProperties
+from win32com.client import Dispatch
 
 
-# fig.tight_layout()
+wb = Workbook()
 
-# plt.show()
+sheet = wb['Sheet']
+
+data = [('Month', 'Type', 'CRITICAL', 'HIGH', 'MEDIUM', 'LOW'), ('March 2022', 'Brute Force Login Attack Failed', '', '', 1, ''), ('', 'Inbound Communication With Blacklist IP Address', '', '', 5, ''), ('April 2022', 'Inbound Communication With Blacklist IP Address', '', '', 4, '')]
+# insert data to excel
+for item in data:
+    sheet.append(item)
+
+# declare chart ty Bar
+
+
+chart1 = BarChart()
+chart1.type = "col"
+chart1.style = 1
+chart1.title = "Bar Chart"
+chart1.grouping = "stacked"
+chart1.overlap = 100
+chart1.y_axis.scaling.min = 0
 
 
 
-arrays = [['Jan', 'Jan', 'Jan', 'Feb', 'Feb', 'Feb'],
-          ['inbound', 'outbound', 'port', 'inbound', 'outbound', 'port']]
-colums = pd.MultiIndex.from_tuples(list(zip(*arrays)))
-index = ['crit','high','med','low']
-df = pd.DataFrame(np.random.randint(0, 1, size=(len(index), len(arrays[1]))),index=index, columns=colums)
-df.loc[df['Jan']['inbound'],'crit'] = '1' 
-print(df)
+# define chart bar
+data = Reference(sheet, min_col=3, min_row=1, max_row=len(data), max_col=6)
+cats = Reference(sheet, min_col=1, min_row=2, max_row=len(data), max_col=2)
+chart1.add_data(data, titles_from_data=True)
+chart1.set_categories(cats)
+chart1.shape = 1
+
+# colors lenend level
+critical = chart1.series[0]
+props = GraphicalProperties(solidFill="7030A0")
+critical.graphicalProperties = props
+
+high = chart1.series[1]
+props = GraphicalProperties(solidFill="FF0000")
+high.graphicalProperties = props
+
+medium = chart1.series[2]
+props = GraphicalProperties(solidFill="FFC000")
+medium.graphicalProperties = props
+
+low = chart1.series[3]
+props = GraphicalProperties(solidFill="70AD47")
+low.graphicalProperties = props
+
+# set lenend position
+chart1.legend.position = 'b'
+chart1.dataLabels = DataLabelList()
+chart1.dataLabels.showVal = True
+
+# {'r', 'l', 'b', 'ctr', 'outEnd', 't', 'inBase', 'bestFit', 'inEnd'}
+chart1.dataLabels.position = 'ctr'
+
+# save chart
+sheet.add_chart(chart1, "M2")
+
+wb.save('sample.xlsx')
+
+# save image
+app = Dispatch("Excel.Application")
+workbook_file_name = 'D:/INET-MS/Auto report/GitHub/WebReport/code_krit/report_SOC/sample.xlsx'
+
+workbook = app.Workbooks.Open(Filename=workbook_file_name)
+
+app.DisplayAlerts = False
+
+i = 1
+for sheet in workbook.Worksheets:
+    for chartObject in sheet.ChartObjects():
+        chartObject.Chart.Export('D:/INET-MS/Auto report/GitHub/WebReport/code_krit/report_SOC/' + str(i) + '.png')
+        i += 1
+workbook.Close(SaveChanges=False, Filename=workbook_file_name)
+
+# os.system("sample.xlsx")
